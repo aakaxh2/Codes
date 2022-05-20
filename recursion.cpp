bd66890f6b8c971c1ce81a4ce4b9fc4b22ac00cf
Aakash Bishnoi <58396391+A-kash02@users.noreply.github.com>
2022-05-20 11:23:54 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -37,4 +37,38 @@ public:
 
 // ------------------------------------- end -----------------------------
 
+// count subsets for coins change : https://leetcode.com/problems/coin-change-2/
+
+const int N=305;
+const int M=5005;
+class Solution {
+public:
+    int dp[N][M];
+    int fun(vector<int>&v, int x, int i)
+    {
+        if(x==0) return 1;
+        if(x<0 || i>=v.size()) return 0;
+        
+        if(dp[i][x]!=-1) return dp[i][x];
+        
+        int y=0;
+        if(v[i]<=x) y=fun(v,x-v[i],i);
+        y+=fun(v,x,i+1);
+        
+        return dp[i][x]=y;
+    }
+    
+    int change(int amount, vector<int>& coins) {
+        
+        if(amount==0) return 1;
+        
+        memset(dp,-1,sizeof(dp));
+        return fun(coins, amount, 0);
+    }
+};
+
+
+// ------------------end--------------------
+
+
 
